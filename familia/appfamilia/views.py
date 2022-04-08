e7b61a30c45642820b6ae0738aa74a568bda993a
Usdatabank <sebastian@usdatabank.com.ar>
2022-04-07 22:11:31 -0300

    Cambios menores

--- familia/appfamilia/views.py
+++ familia/appfamilia/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import familiares
 
 # Create your views here.
 def index(request):
     return HttpResponse("Mensaje de prueba")
     
 def integrante(request):
-  return HttpResponse()
+  familiar = familiares.objects.filter(nombre__contains='andrea')
+  return render(request, 'integrantes.html', {})
